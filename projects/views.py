ecec1ea437b04f87109d69274893df112c4f7cf1
Mostafa-DE <mostafafayyado1@gmail.com>
2021-12-15 11:57:37 +0200

    add review form, add logic for count the vote, fix som problem with template :)

--- projects/views.py
+++ projects/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, redirect
 from .models import Project
-from .forms import ProjectForm
+from .forms import ProjectForm, ReviewForm
 from django.contrib.auth.decorators import login_required
 from .utils import searchProjects, paginationProjects
+from django.contrib import messages
 
 def homePage(request):
     projects , search_query = searchProjects(request)
@@ -20,8 +21,21 @@ def homePage(request):
 
 def projects(request, pk):
     projectObj = Project.objects.get(id=pk)
+    form = ReviewForm()
     
-    return render(request, 'projects/single-project.html', { "projects": projectObj})
+    if(request.method == 'POST'):
+        form = ReviewForm(request.POST)
+        review = form.save(commit=False)
+        review.project = projectObj
+        review.owner = request.user.profile
+        review.save()
+        
+        projectObj.getVoteCount
+        
+        messages.success(request, 'Your review was successfully added')
+        return redirect('project', pk=projectObj.id)
+    
+    return render(request, 'projects/single-project.html', { "projects": projectObj, "form": form})
 
 
 
--- users/views.py
+++ users/views.py
@@ -45,7 +45,7 @@ def loginUser(request):
         return redirect("profiles")
     
     if request.method == 'POST':
-        username= request.POST['username']
+        username= request.POST['username'].lower()
         password= request.POST['password']
         
         try:
@@ -57,7 +57,7 @@ def loginUser(request):
         
         if user is not None:
             login(request, user)
-            return redirect('profiles')
+            return redirect(request.GET['next'] if 'next' in request.GET else 'account')
         else:
             messages.error(request,"Username or password does not exist")
             
